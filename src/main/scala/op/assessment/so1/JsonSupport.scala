174e13d7eb8a1cb52fb63c0f9c9b4c4ffc297024
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2018-03-13 11:27:48 +0200

    So1 trial working day: initial commit.

---
+++ src/main/scala/op/assessment/so1/JsonSupport.scala
@@ -0,0 +1,17 @@
+package op.assessment.so1
+
+//#json-support
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import op.assessment.so1.UserRegistryActor.ActionPerformed
+import spray.json.DefaultJsonProtocol
+
+trait JsonSupport extends SprayJsonSupport {
+
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
+//#json-support
